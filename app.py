615e37216a9e4aa9d85d40f56f4866704efe193f
Jaz Gamarra <j444min@gmail.com>
2023-04-14 12:14:01 -0400

    taller feliz uwu

---
+++ app.py
@@ -0,0 +1,15 @@
+# Importar flask 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index(): 
+    return render_template('index.html')
+
+@app.route('/ejemplo')
+def ejemplo(): 
+    return render_template('ejemplo.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
